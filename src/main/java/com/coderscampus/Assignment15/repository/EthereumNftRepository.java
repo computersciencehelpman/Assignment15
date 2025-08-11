04ba7d48c37da608ac7daad9e8cb79e3b9fdd0f2
Saurav Bhatt <55709609+computersciencehelpman@users.noreply.github.com>
2025-08-11 00:32:27 -0400

    Delete src/main/java/com/coderscampus/Assignment15/repository directory

--- src/main/java/com/coderscampus/Assignment15/repository/EthereumNftRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coderscampus.Assignment15.repository;
-
-import com.coderscampus.Assignment15.domain.EthereumNftRecommendation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EthereumNftRepository extends JpaRepository<EthereumNftRecommendation, Long> {
-    List<EthereumNftRecommendation> findAllByOrderByCreatedAtDesc();
-}
